909daa771fde3559b6af6b0a7404e147e211a65d
faraon <faraon@lan>
2018-02-11 23:19:57 +0100

    updated prop-types for react 16 and material-ui 1.0

--- src/Alerts.jsx
+++ src/Alerts.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Snackbar from 'material-ui/Snackbar';
 import Dialog from 'material-ui/Dialog';
